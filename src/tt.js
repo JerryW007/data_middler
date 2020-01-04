8d080d9e5ef05a36df436588e7f736b1f2afefa7
wangchao3 <wangchao3@ihangmei.com>
2020-01-04 14:26:54 +0800

    sss

--- src/tt.js
+++ src/tt.js
@@ -1,6 +1,8 @@
 /*
  * @Author: wangchao 
  * @Date: 2020-01-04 11:47:44 
- * @Last Modified by:   wangchao 
- * @Last Modified time: 2020-01-04 11:47:44 
+ * @Last Modified by: wangchao
+ * @Last Modified time: 2020-01-04 14:26:43
  */
+
+ console.log('test')
