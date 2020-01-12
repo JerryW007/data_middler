54a5dd15203426b631a0f3a90dd6233707a9ad89
wangchao3 <wangchao3@ihangmei.com>
2020-01-12 23:25:57 +0800

    test

--- src/tt.js
+++ src/tt.js
@@ -2,7 +2,22 @@
  * @Author: wangchao 
  * @Date: 2020-01-04 11:47:44 
  * @Last Modified by: wangchao
- * @Last Modified time: 2020-01-04 14:26:43
+ * @Last Modified time: 2020-01-12 23:25:43
  */
 
- console.log('test')
+console.log('test')
+
+// test
+const arr = [1, 2, 3, 4, 44, 4];
+arr.forEach(item => {
+    // setTimeout(() => {
+    // console.log(item);
+    // },1000)
+    console.log(item);
+    break;
+});
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    return
+}
+console.log("111")
