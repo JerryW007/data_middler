2cf8c8f79ed619153a865fd9fd14cb9f03579c9f
wangchao3 <wangchao3@ihangmei.com>
2020-03-04 09:50:49 +0800

    add console

--- src/tt.js
+++ src/tt.js
@@ -2,7 +2,8 @@
  * @Author: wangchao 
  * @Date: 2020-01-04 11:47:44 
  * @Last Modified by: wangchao
- * @Last Modified time: 2020-01-04 14:26:43
+ * @Last Modified time: 2020-03-04 09:50:35
  */
 
  console.log('test')
+ console.log('this branch is fixBug1')
